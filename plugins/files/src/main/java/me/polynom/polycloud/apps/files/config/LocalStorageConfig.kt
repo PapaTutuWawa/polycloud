1a5d951a6a483ef9c86718e83dbc5e286bf8ee5f
ostylk <ostylk@googlemail.com>
2026-09-14 15:59:15 +0200

    files: Implement StorageService and StoragePath

--- plugins/files/src/main/java/me/polynom/polycloud/apps/files/config/LocalStorageConfig.kt
+++ plugins/files/src/main/java/me/polynom/polycloud/apps/files/config/LocalStorageConfig.kt
@@ -1,16 +1,18 @@
 package me.polynom.polycloud.apps.files.config
 
+import me.polynom.polycloud.apps.files.storage.StoragePath
 import org.springframework.boot.context.properties.bind.ConstructorBinding
+import java.nio.file.Path
 
 data class LocalStorageConfig @ConstructorBinding constructor(
     /**
      * Absolute path on the host that serves as the root of the backend
      */
-    val path: String,
+    val path: Path,
     /**
      * The path in the virtual file system where this backend exposes its files
      */
-    val mount: String,
+    val mount: StoragePath,
     /**
      * Whether file access is separated between users or a common file pool is accessed (use with caution!)
      */
