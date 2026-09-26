c0a478aeb922c62cc764b946325b352bc01a6426
Alexander "PapaTutuWawa <papatutuwawa@polynom.me>
2026-09-26 19:52:29 +0200

    core: Fix CORS header

--- core/src/main/kotlin/me/polynom/polycloud/security/CorsFilter.kt
+++ core/src/main/kotlin/me/polynom/polycloud/security/CorsFilter.kt
@@ -15,6 +15,9 @@ import org.springframework.stereotype.Component
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 class CorsFilter : Filter {
+    /** Allowed HTTP methods. */
+    private val allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS").joinToString(",")
+
     override fun doFilter(
         request: ServletRequest,
         response: ServletResponse,
@@ -26,8 +29,8 @@ class CorsFilter : Filter {
         }
 
         response.setHeader("Access-Control-Allow-Origin", "*")
-        response.setHeader("Access-Control-Request-Method", "*")
         response.setHeader("Access-Control-Allow-Headers", "*")
+        response.setHeader("Access-Control-Allow-Methods", allowedMethods)
 
         // If we have a CORS preflight request, then just terminate here and do not run anything else.
         if (request.method == "OPTIONS") {
